6909e2794b132b9fd3d3f69035af8072be1f5cbf
Jaeyun Kim <jaeyunkim@snu.ac.kr>
2024-09-14 12:43:07 +0900

    update style

--- src/app/components/lightbox.tsx
+++ src/app/components/lightbox.tsx
@@ -1,7 +1,7 @@
 // components/Lightbox.tsx
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 interface LightboxProps {
   imageUrl: string;
@@ -11,16 +11,25 @@ interface LightboxProps {
 export default function Lightbox({ imageUrl, altText }: LightboxProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false); // 이미지 로딩 상태
 
   const handleImageClick = () => {
     setIsModalOpen(true);
     setIsHovered(false);
+    document.body.style.overflow = 'hidden'; // 모달이 열리면 스크롤 방지
   };
 
   const closeModal = () => {
     setIsModalOpen(false);
     setIsHovered(false);
+    document.body.style.overflow = ''; // 모달이 닫히면 스크롤 활성화
   };
+
+  useEffect(() => {
+    return () => {
+      document.body.style.overflow = ''; // 컴포넌트 언마운트 시 스크롤 복구
+    };
+  }, []);
 
   return (
     <div
@@ -28,35 +37,38 @@ export default function Lightbox({ imageUrl, altText }: LightboxProps) {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
+      {!isLoaded && (
+        <div className="flex items-center justify-center h-[80vh] bg-gray-200 absolute inset-0">
+          <p className="text-lg text-gray-500">Loading...</p>
+        </div>
+      )}
+      {/* 이미지 */}
       <img
         src={imageUrl}
         alt={altText}
         className={`w-full h-auto max-h-[80vh] object-contain cursor-pointer transition-transform duration-300 ${
           isHovered ? 'scale-105' : 'scale-100'
-        }`}
+        } ${isLoaded ? 'opacity-100' : 'opacity-0'}`} // 로딩이 완료되면 투명도 조정
+        onLoad={() => setIsLoaded(true)} // 이미지가 로드되면 상태 업데이트
         onClick={handleImageClick}
       />
-      {isHovered && (
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white text-medium font-medium transition-opacity duration-200 opacity-100 pointer-events-none">
+      
+      {isHovered && isLoaded && (
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center text-white font-medium transition-opacity duration-200 opacity-100 pointer-events-none">
             크게 보려면 클릭하세요
         </div>
       )}
       {isModalOpen && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 p-4"
+          className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
           onClick={closeModal}
         >
           <div className="relative max-w-full max-h-full">
-            <button
-              className="absolute top-2 right-2 text-white bg-black bg-opacity-50 rounded-full p-2"
-              onClick={closeModal}
-            >
-              &times;
-            </button>
             <img
               src={imageUrl}
               alt={altText}
-              className="w-auto h-auto max-w-full max-h-screen object-contain"
+              onClick={closeModal}
+              className="w-auto h-auto max-w-full max-h-screen object-contain p-5"
             />
           </div>
         </div>

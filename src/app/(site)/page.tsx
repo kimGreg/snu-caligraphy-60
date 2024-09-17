99c9884279757c30aa58e2a1645a8921779d839e
Jaeyun Kim <jaeyunkim@snu.ac.kr>
2024-09-17 16:35:33 +0900

    update file

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -5,13 +5,11 @@ import Image from "next/image"
 
 export default function AboutPage() {
   return (
-    <section className="py-10 bg-gray-50">
-      <div className="container mx-auto px-4">
-        <div className="flex justify-center">
+    <section className="py-10 bg-gray-50  w-full ">
+      <div className="container mx-auto w-full px-2 lg:px-6 ">
+        <div className="flex justify-center px-0 lg:px-8">
             <Image src={title} alt="서울대학교 서예회 창립60주년 기념전" height={500} width={1000}></Image>
         </div>
-        
-        
         
       </div>
     </section>

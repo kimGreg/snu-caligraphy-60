af319d5265697cc292369dddee2802436018e4b9
Jaeyun Kim <jaeyunkim@snu.ac.kr>
2024-09-26 22:03:14 +0900

    update person

--- src/app/(site)/contributor/page.tsx
+++ src/app/(site)/contributor/page.tsx
@@ -10,84 +10,84 @@ export default function InstructorsPage() {
                 재학생
                 </div>
                 <div className="pb-10 text-justify">
-                권대헌&nbsp;
-                권영빈&nbsp;
-                권정해&nbsp;
-                김나영&nbsp;
-                김민지&nbsp;
-                김태륜&nbsp;
-                박경준&nbsp;
-                배승빈&nbsp;
-                뱅상&nbsp;
-                신지우&nbsp;
-                옥윤찬&nbsp;
-                유경민&nbsp;
-                이동훈&nbsp;
-                이상연&nbsp;
-                이시은&nbsp;
-                이하은&nbsp;
-                장능관&nbsp;
-                장세은&nbsp;
-                전지현&nbsp;
-                정재헌&nbsp;
+                권대헌
+                권영빈
+                권정해
+                김나영
+                김민지
+                김태륜
+                박경준
+                배승빈
+                뱅상
+                신지우
+                옥윤찬
+                유경민
+                이동훈
+                이상연
+                이시은
+                이하은
+                장능관
+                장세은
+                전지현
+                정재헌
                 조상연
                 </div>
                 <div className="pb-3">
                     졸업생
                 </div>
                 <div className="pb-10 text-justify">
-                강명희&nbsp;
-                김진기&nbsp;
-                남동신&nbsp;
-                故노태천&nbsp;
-                박관규&nbsp;
-                박문주&nbsp;
-                박혜원&nbsp;
-                방근화&nbsp;
-                변지원&nbsp;
-                성환진&nbsp;
-                소수경&nbsp;
-                안미정&nbsp;
-                양재균&nbsp;
-                엄태영&nbsp;
-                오희정&nbsp;
-                이강록&nbsp;
-                이기영&nbsp;
-                이나라&nbsp;
-                이한결&nbsp;
-                장기택&nbsp;
-                장덕수&nbsp;
-                정광수&nbsp;
-                정종섭&nbsp;
-                조병훈&nbsp;
-                최고&nbsp;
-                최유진&nbsp;
-                허선&nbsp;
+                강명희
+                김진기
+                남동신
+                故노태천
+                박관규
+                박문주
+                박혜원
+                방근화
+                변지원
+                성환진
+                소수경
+                안미정
+                양재균
+                엄태영
+                오희정
+                이강록
+                이기영
+                이나라
+                이한결
+                장기택
+                장덕수
+                정광수
+                정종섭
+                조병훈
+                최고
+                최유진
+                허선
                 홍은정
                 </div>
                 <div className="pb-3">
                     지도강사
                 </div>
                 <div className="pb-10 text-justify">
-                故김응현&nbsp;
-                박용설&nbsp;
-                남기매&nbsp;
-                박주영&nbsp;
+                故김응현
+                박용설
+                남기매
+                박주영
                 정범훈
                 </div>
                 <div className="pb-3">
                     찬조
                 </div>
                 <div className="pb-10 text-justify">
-                권숙일&nbsp;
-                권순민&nbsp;
-                김동진&nbsp;
-                손희정&nbsp;
-                양일모&nbsp;
-                이기석&nbsp;
-                이성우&nbsp;
-                이정우&nbsp;
-                최주완&nbsp;
+                권숙일
+                권순민
+                김동진
+                손희정
+                양일모
+                이기석
+                이성우
+                이정우
+                최주완
                 홍광현
                 </div>
             </div>
